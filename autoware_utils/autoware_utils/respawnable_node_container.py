21b05c46a84a5ce35fc62035f5bfa2f84de37e64
bartek-kc <bartosz.kozlowiec@robotec.ai>
2022-10-26 18:36:00 +0200

    Added Python module with delegate class for launching respawnable node containers

---
+++ autoware_utils/autoware_utils/respawnable_node_container.py
@@ -0,0 +1,29 @@
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+from typing import List
+from typing import Optional
+
+from launch.some_substitutions_type import SomeSubstitutionsType
+
+class RespawnableNodeContainer(ComposableNodeContainer):
+    """
+    Action that executes a container node for composable ROS nodes with fixed
+    respawn flag and time.
+    """
+
+    def __init__(
+        self,
+        *,
+        name: SomeSubstitutionsType,
+        namespace: SomeSubstitutionsType,
+        composable_node_descriptions: Optional[List[ComposableNode]] = None,
+        respawn = True,
+        respawn_delay = 0.5,
+        **kwargs
+    ) -> None:
+        super().__init__(
+            name=name,
+            namespace=namespace,
+            respawn=respawn,
+            respawn_delay=respawn_delay,
+            **kwargs)
