8c79547fc69b9cdb332898b7902194cff7d57558
Mamoru Sobue <mamoru.sobue@tier4.jp>
2023-11-27 19:41:58 +0900

    add AutowareTrafficLight and implicitly_convertible
    Signed-off-by: Mamoru Sobue <mamoru.sobue@tier4.jp>

--- tmp/lanelet2_extension_python/src/regulatory_elements.cpp
+++ tmp/lanelet2_extension_python/src/regulatory_elements.cpp
@@ -8,9 +8,103 @@
 
 #include <boost/python.hpp>
 
+using namespace boost::python;
+
+// from lanelet2_python/src/core.cpp
+
+void formatHelper([[maybe_unused]] std::ostream & os)
+{
+}
+
+template <typename... Args>
+// NOLINTNEXTLINE(readability-identifier-naming)
+void formatHelper(std::ostream & os, const std::string & s, const Args &... Args_)
+{
+  if (!s.empty()) {
+    os << ", ";
+  }
+  os << s;
+  formatHelper(os, Args_...);
+}
+
+template <typename T, typename... Args>
+// NOLINTNEXTLINE(readability-identifier-naming)
+void formatHelper(std::ostream & os, const T & next, const Args &... Args_)
+{
+  os << ", ";
+  os << next;
+  formatHelper(os, Args_...);
+}
+
+template <typename T, typename... Args>
+// NOLINTNEXTLINE(readability-identifier-naming)
+void format(std::ostream & os, const T & first, const Args &... Args_)
+{
+  os << first;
+  formatHelper(os, Args_...);
+}
+
+template <typename... Args>
+// NOLINTNEXTLINE(readability-identifier-naming)
+std::string makeRepr(const char * name, const Args &... Args_)
+{
+  std::ostringstream os;
+  os << name << '(';
+  format(os, Args_...);
+  os << ')';
+  return os.str();
+}
+
+std::string repr(const object & o)
+{
+  object repr = import("builtins").attr("repr");
+  return call<std::string>(repr.ptr(), o);
+}
+
+std::string repr(const lanelet::AttributeMap & a)
+{
+  if (a.empty()) {
+    return {};
+  }
+  return repr(object(a));
+}
+
+std::string repr(const lanelet::RegulatoryElementConstPtrs & regelems)
+{
+  if (regelems.empty()) {
+    return {};
+  }
+  return repr(list(regelems));
+}
+
 BOOST_PYTHON_MODULE(_lanelet2_extension_python_boost_python_regulatory_elements)
 {
-  using namespace boost::python;
+  // autoware_traffic_light
+  class_<
+    lanelet::autoware::AutowareTrafficLight, boost::noncopyable,
+    std::shared_ptr<lanelet::autoware::AutowareTrafficLight>, bases<lanelet::TrafficLight>>(
+    "AutowareTrafficLight", "autoware traffic light regulatory element", no_init)
+    .def(
+      "__init__", make_constructor(
+                    &lanelet::autoware::AutowareTrafficLight::make, default_call_policies(),
+                    (arg("Id"), arg("attributes"), arg("trafficLights"),
+                     arg("stopline") = lanelet::Optional<lanelet::LineString3d>(),
+                     arg("lightBulbs") = lanelet::LineString3d({}))))
+    .def(
+      "lightBulbs",
+      +[](lanelet::autoware::AutowareTrafficLight & self) { return self.lightBulbs(); })
+    .def("addLightBulbs", &lanelet::autoware::AutowareTrafficLight::addLightBulbs)
+    .def("removeLightBulbs", &lanelet::autoware::AutowareTrafficLight::removeLightBulbs)
+    .def(
+      "__repr__", +[](lanelet::autoware::AutowareTrafficLight & r) {
+        return makeRepr(
+          "AutowareTrafficLight", r.id(), repr(dict(r.constData()->parameters)),
+          repr(r.attributes()));
+      });
+  implicitly_convertible<
+    std::shared_ptr<lanelet::autoware::AutowareTrafficLight>, lanelet::RegulatoryElementPtr>();
+
+  // crosswalk
   class_<
     lanelet::autoware::Crosswalk, boost::noncopyable, std::shared_ptr<lanelet::autoware::Crosswalk>,
     bases<lanelet::RegulatoryElement>>(
@@ -28,5 +122,12 @@ BOOST_PYTHON_MODULE(_lanelet2_extension_python_boost_python_regulatory_elements)
       "crosswalkLanelet",
       +[](lanelet::autoware::Crosswalk & self) { return self.crosswalkLanelet(); })
     .def("addCrosswalkArea", &lanelet::autoware::Crosswalk::addCrosswalkArea)
-    .def("removeCrosswalkArea", &lanelet::autoware::Crosswalk::removeCrosswalkArea);
+    .def("removeCrosswalkArea", &lanelet::autoware::Crosswalk::removeCrosswalkArea)
+    .def(
+      "__repr__", +[](lanelet::autoware::Crosswalk & r) {
+        return makeRepr(
+          "Crosswalk", r.id(), repr(dict(r.constData()->parameters)), repr(r.attributes()));
+      });
+  implicitly_convertible<
+    std::shared_ptr<lanelet::autoware::Crosswalk>, lanelet::RegulatoryElementPtr>();
 }
