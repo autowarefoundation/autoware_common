131e976f47fa2c13e3856bde2e1a6c7bad5edb34
Takayuki Murooka <takayuki5168@gmail.com>
2024-06-11 23:19:05 +0900

    remove autoware_utils
    Signed-off-by: Takayuki Murooka <takayuki5168@gmail.com>

--- tmp/lanelet2_extension/lib/query.cpp
+++ tmp/lanelet2_extension/lib/query.cpp
@@ -28,7 +28,6 @@
 #include "lanelet2_extension/utility/utilities.hpp"
 
 #include <Eigen/Eigen>
-#include <autoware_utils/autoware_utils.hpp>
 
 #include <lanelet2_core/LaneletMap.h>
 #include <lanelet2_core/geometry/Lanelet.h>
@@ -49,6 +48,21 @@
 #include <vector>
 
 using lanelet::utils::to2D;
+
+namespace
+{
+double normalize_radian(const double rad, const double min_rad = -M_PI)
+{
+  const auto max_rad = min_rad + 2 * M_PI;
+
+  const auto value = std::fmod(rad, 2 * M_PI);
+  if (min_rad <= value && value < max_rad) {
+    return value;
+  }
+
+  return value - std::copysign(2 * M_PI, value);
+}
+}  // namespace
 
 namespace lanelet::utils
 {
@@ -892,7 +906,7 @@ bool query::getClosestLanelet(
       if (!segment.empty()) {
         double segment_angle = std::atan2(
           segment.back().y() - segment.front().y(), segment.back().x() - segment.front().x());
-        angle_diff = std::abs(autoware_utils::normalize_radian(segment_angle - pose_yaw));
+        angle_diff = std::abs(normalize_radian(segment_angle - pose_yaw));
       }
       if (angle_diff < min_angle) {
         min_angle = angle_diff;
@@ -954,7 +968,7 @@ bool query::getClosestLaneletWithConstrains(
       const auto & distance = llt_pair.second;
 
       double lanelet_angle = getLaneletAngle(llt_pair.first, search_pose.position);
-      double angle_diff = std::abs(autoware_utils::normalize_radian(lanelet_angle - pose_yaw));
+      double angle_diff = std::abs(normalize_radian(lanelet_angle - pose_yaw));
 
       if (angle_diff > std::abs(yaw_threshold)) continue;
       if (min_distance < distance) break;
