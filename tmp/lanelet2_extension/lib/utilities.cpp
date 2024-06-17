fc7063d318c7fd259ee2663a3fed9cce2a9ce561
Takayuki Murooka <takayuki5168@gmail.com>
2024-06-17 12:22:09 +0900

    Update tmp/lanelet2_extension/lib/utilities.cpp
    Co-authored-by: Ryohsuke Mitsudome <43976834+mitsudome-r@users.noreply.github.com>

--- tmp/lanelet2_extension/lib/utilities.cpp
+++ tmp/lanelet2_extension/lib/utilities.cpp
@@ -508,21 +508,20 @@ void overwriteLaneletsCenterline(
     if (force_overwrite) {
       const auto fine_center_line = generateFineCenterline(lanelet_obj, resolution);
       lanelet_obj.setCenterline(fine_center_line);
-    } else {
-      if (use_waypoints) {
-        if (lanelet_obj.hasCustomCenterline()) {
-          const auto & centerline = lanelet_obj.centerline();
-          lanelet_obj.setAttribute("waypoints", centerline.id());
-        }
-
-        const auto fine_center_line = generateFineCenterline(lanelet_obj, resolution);
-        lanelet_obj.setCenterline(fine_center_line);
-      } else {
-        if (!lanelet_obj.hasCustomCenterline()) {
-          const auto fine_center_line = generateFineCenterline(lanelet_obj, resolution);
-          lanelet_obj.setCenterline(fine_center_line);
-        }
+    if (force_overwrite) {
+      const auto fine_center_line = generateFineCenterline(lanelet_obj, resolution);
+      lanelet_obj.setCenterline(fine_center_line);
+    } else if (use_waypoints) {
+      if (lanelet_obj.hasCustomCenterline()) {
+        const auto & centerline = lanelet_obj.centerline();
+        lanelet_obj.setAttribute("waypoints", centerline.id());
       }
+
+      const auto fine_center_line = generateFineCenterline(lanelet_obj, resolution);
+      lanelet_obj.setCenterline(fine_center_line);
+    } else if (!lanelet_obj.hasCustomCenterline()) {
+      const auto fine_center_line = generateFineCenterline(lanelet_obj, resolution);
+      lanelet_obj.setCenterline(fine_center_line);
     }
   }
 }
